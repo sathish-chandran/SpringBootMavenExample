21d80311900a6ba2e00a0895668e476d5d4f6443
Sathish Chandran <sathishchandran@Sathishs-MacBook-Pro.local>
2021-03-07 14:04:36 -0800

    Updated controller to fix error

--- src/main/java/com/sc/application/HomeController.java
+++ src/main/java/com/sc/application/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public String index() {
-        return "Welcome to GitHub Act!";
+        return "Welcome to GitHub Actions!";
     }
 
 }
